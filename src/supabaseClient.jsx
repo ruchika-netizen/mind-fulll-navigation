8e12a3325afabb574dcf01fc527d645d0d2eae3a
ruchika-netizen <ruchika@canws.com>
2026-05-06 15:44:11 +0530

    latest

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrs = "https://esfmcrpslwwmjysbloqf.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZm1jcnBzbHd3bWp5c2Jsb3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0ODQ0NjMsImV4cCI6MjA5MzA2MDQ2M30.RkHZhCheRsoYVskSHP1c2BvivUrzIsR1CK6edx4VZyA"
+const supabaseUrs = "https://yqvevufukdoeyhxizbtb.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxdmV2dWZ1a2RvZXloeGl6YnRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MDkwODUsImV4cCI6MjA4OTM4NTA4NX0.r-1T_yaE2DgZj4Ys3lT3vAMWLwFGR3UvGzT9SHFlZpI"
 
 export const supabase = createClient(supabaseUrs, supabaseKey)
